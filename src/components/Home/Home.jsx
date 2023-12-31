f61d874a8140bd12a52e851b3442a0f9cfec7ea5
Danish Ansari <90023391+Danish1111ansari@users.noreply.github.com>
2023-12-31 22:11:41 +0530

    3d part of home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,38 +1,113 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import "./Home.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import moonImage from "../../Images/moon.jpg";
+import spaceImage from "../../Images/space.jpg";
+import venusImage from "../../Images/venus.jpg";
 const Home = () => {
-    useEffect(() => { 
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        const canvas = document.querySelector(".homeCanvas");
-        const renderer = new THREE.WebGLRenderer({ canvas });
+  useEffect(() => {
 
+    const textureLoader = new THREE.TextureLoader();
 
-        const moonGeometry = new THREE.SphereGeometry(3, 16, 16);
-        const moonMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-        const moon = new THREE.Mesh(moonGeometry, moonMaterial);
-        scene.add(moon);
+    const moonTexture = textureLoader.load(moonImage);
+    const venusTexture = textureLoader.load(venusImage);
+    const spaceTexture = textureLoader.load(spaceImage);
 
-        camera.position.z = 10;
-        const animate = () => {
-            requestAnimationFrame(animate);
-            moon.rotation.y += 0.01;
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            renderer.render(scene, camera);
-        };
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      80,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.set(4, 4, 8);
 
-        animate();
-        
+    const canvas = document.querySelector(".homeCanvas");
+    const renderer = new THREE.WebGLRenderer({ canvas });
 
-    },[])
+    const moonGeometry = new THREE.SphereGeometry(1.7, 64, 64);
+    const moonMaterial = new THREE.MeshBasicMaterial({ map: moonTexture });
+    const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+
+    moon.position.set(1, 3, 0);
+
+    const venusGeometry = new THREE.SphereGeometry(3, 64, 64);
+    const venusMaterial = new THREE.MeshBasicMaterial({ map: venusTexture });
+    const venus = new THREE.Mesh(venusGeometry, venusMaterial);
+    venus.position.set(9, 7, 5);
+
+    // const pointLight = new THREE.PointLight(0xffffff, 100);
+    // const pointLight2 = new THREE.PointLight(0xffffff, 20);
+
+    // pointLight.position.set(9, 7, 5);
+    // pointLight2.position.set(1,3,0);
+
+    scene.add(moon);
+    scene.add(venus);
+    // scene.add(pointLight);
+    // scene.add(pointLight2);
+    scene.background = spaceTexture;
+
+    const constSpeed = 0.01;
+    window.addEventListener("mousemove", (e) => {
+      if (e.clientX <= window.innerWidth / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y += constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y += constSpeed;
+      }
+
+      if (e.clientX > window.innerWidth / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y -= constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y -= constSpeed;
+      }
+
+      if (e.clientY > window.innerHeight / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y += constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y += constSpeed;
+      }
+
+      if (e.clientY <= window.innerHeight / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y -= constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y -= constSpeed;
+      }
+    });
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      moon.rotation.y += 0.001;
+      venus.rotation.y += 0.001;
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    // return window.addEventListener("scroll", () => {
+    //   camera.rotation.z = window.scrollY * 0.001;
+    //   camera.rotation.y = window.scrollY * 0.003;
+
+    //   const skillsBox = document.getElementById("homeskillsBox");
+
+    //   if (window.scrollY > 1500) {
+    //     skillsBox.style.animationName = "homeskillsBoxAnimationOn";
+    //   } else {
+    //     skillsBox.style.animationName = "homeskillsBoxAnimationOff";
+    //   }
+    // });
+  }, []);
   return (
     <div className="home">
-          <canvas className="homeCanvas">
-              
-      </canvas>
+      <canvas className="homeCanvas"></canvas>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
